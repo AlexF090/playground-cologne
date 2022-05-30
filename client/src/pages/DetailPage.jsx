2d622230401a56bcf5b58e3775f78d3302c90d6e
AlexF090 <alexander.fischenich@pm.me>
2022-05-30 22:36:37 +0200

    small change in DetailPage

--- client/src/pages/DetailPage.jsx
+++ client/src/pages/DetailPage.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import PlaygroundDetail from '../components/PlaygroundDetail/PlaygroundDetail.jsx';
 
-function DetailPage({ playgrounds }) {
+function DetailPage() {
   const { id } = useParams();
   const [currentPlayground, setCurrentPlayground] = useState();
   const url = `https://geoportal.stadt-koeln.de/arcgis/rest/services/freizeit_natur_sport/spielangebote/MapServer/0/query?where=objectid+is+not+null&text=&objectIds=${id}&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&distance=&units=esriSRUnit_Foot&relationParam=&outFields=*&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=4326&havingClause=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&historicMoment=&returnDistinctValues=false&resultOffset=&resultRecordCount=&returnExtentOnly=false&datumTransformation=&parameterValues=&rangeValues=&quantizationParameters=&featureEncoding=esriDefault&f=json`;
