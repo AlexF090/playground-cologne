30ec3d7404dc0fa3d49a89a78a3b7774bf085db4
AlexF090 <alexander.fischenich@pm.me>
2022-07-06 13:53:04 +0200

    Add the first fetch and add manual playgrounds

--- server/server.js
+++ server/server.js
@@ -3,12 +3,11 @@ import path from 'path';
 import api from './routes/api.js';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5005;
 
 app.use(express.json());
 
